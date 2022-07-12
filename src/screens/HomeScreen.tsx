e2b579847b6366b44cbcb1f9453ce344c074dff0
Ivan Lara <ukikalix@gmail.com>
2022-07-12 10:59:52 -0600

    initial commit

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const HomeScreen = () => {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <h1>Hola Dev Blog</h1>
+        </div>
+    );
+};
